759ada79063e86377e1d240c96e8c04c0806debb
lty <lty@lty-PC.gateway.2wire.net>
2012-08-09 01:42:19 -0400

    outrageous push

--- includes/friendslist.php
+++ includes/friendslist.php
@@ -6,9 +6,11 @@
 include_once 'database/friends.php';
 include_once 'database/profile.php';
 
+//get the list of friends' uid
 $frd_list= getFriends();
 $frdId=-1;
 
+//iterate over each userid and display the info
 foreach ($frd_list as $frd)
 {
     $frdId=$frd;

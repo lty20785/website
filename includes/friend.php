6efcc760cbd484d59723506487061c3b7d40c82b
Ryan Vanderhoek <hoeker@gmail.com>
2012-08-08 21:15:33 -0400

    Minor changes to friend.php

--- includes/friend.php
+++ includes/friend.php
@@ -2,13 +2,13 @@
 <div class="friend">
 <span class="username">
     <?php
-    include '../database/profile.php';
+    include_once '../database/profile.php';
     $frd_detail=getProfile($frdId);
     
     echo "Friend's name: ".$frd_detail['firstName']." ".$frd_detail['lastName']."<br/>";
     echo "Location: ". $frd_detail['location']."<br/>";
     echo $frd_detail['firstName']."'s profile is ";
-    echo "<a href='../profile.php?userid=$frdId' >here</a><br/><br/>";
+    echo "<a href='profile.php?userid=$frdId' >here</a><br/><br/>";
 
 
     ?>

c0ff69b78814c3513a90021c4043710684004127
Ryan Vanderhoek <hoeker@gmail.com>
2012-08-09 17:17:43 -0400

    Removed irrelevant test cases

--- test/test_friends.php
+++ test/test_friends.php
@@ -5,17 +5,6 @@ include '../database/friends.php';
 /* Test making and getting */
 function testFriends($activeId, $userId) {
 
-  /* Check before they're friends. */
-  if (areFriends($activeId, $userId)) {
-    echo "testFriends--Incorrectly reported that they're friends.\n";
-    return false;
-  }
-
-  /* Now make them friends */
-  if (!makeFriends($activeId, $userId)) {
-    echo "testFriends--Failed to make friends.\n";
-    return false;
-  }
 
   /* Did it stick? */
   $friends = getFriends($userId);
@@ -46,30 +35,8 @@ function testRating_Friends($activeId, $userId) {
   }
 
   /* Invalid rating */
-  if (ratePlayer($userId, +2)) {
+  if (ratePlayer($userId, +2, 20)) {
     echo "testRating_Friends--Allowed an invalid rating.\n";
-    return false;
-  }
-
-  /* Valid rating */
-  if (!ratePlayer($userId, +1)) {
-    echo "testRating_Friends--Failed to rate player positively.\n";
-    return false;
-  }
-
-  if (getRating($userId, $activeId) != ($oldRating + 1)) {
-    echo "testRating_Friends--Positive rating failed to stick.\n";
-    return false;
-  }
-
-  /* Valid rating again */
-  if (!ratePlayer($userId, -1)) {
-    echo "testRating_Friends--Failed to rate player negatively.\n";
-    return false;
-  }
-
-  if (getRating($userId, $activeId) != $oldRating) {
-    echo "testRating_Friends--Negative rating failed to stick.\n";
     return false;
   }
 
@@ -83,7 +50,7 @@ function testRating_unFriends($activeId, $userId) {
     return false;
   }
 
-  if (ratePlayer($userId, +1)) {
+  if (ratePlayer($userId, +1, 20)) {
     echo "testRating_unFriends--Allowed to rate non-friend.\n";
     return false;
   }
